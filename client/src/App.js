a5607f0573b9fc910ed4407f804906c5cafcbec3
reimon <reimonsk8@gmail.com>
2020-07-02 19:06:12 -0700

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import mondo0 from './assets/mondo0.png';
 import mondo1 from './assets/mondo1.png';
+import discomap from './assets/DISCO MONDO MAPA.png'
+import discologo from './assets/logo disco mondo solido.png'
+import discologocon from './assets/isotipo contorno.png'
 import './styles/App.css';
 import './styles/magnetic.css';
+import $ from 'jquery';
 
 class App extends React.Component{
   constructor(props){
@@ -16,13 +20,98 @@ class App extends React.Component{
       .then(res=> res.setState({apiResponse: res}));
   }
 
+
+
+
+
+
+
   componetWillMount(){
     this.callAPI();
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   componentDidMount(){
     
     console.clear();
+    
+    var imgs = [ discologo, discologocon]
+    var spins = 0;
+    var item = $("#disco-logo") 
+    spin(item);
+    
+    function spin(target){
+      target.css({"animation":"start 1s linear"});
+      setTimeout(function(){
+        target.css({"animation":"end 1s linear"});
+        target.attr("src",imgs[spins++ % imgs.length])
+      },1000);
+      
+      setTimeout(function(){
+        spin(target);
+      },2000);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     const { gsap } = window;
 
@@ -109,19 +198,45 @@ class App extends React.Component{
         backgroundColor: "transparent",
       });
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   }
 
 render(){
   return (
     <div className="App">
       <header className="App-header">
+        <img src={discologo} id="disco-logo" alt="logo" />
+        <img src={discomap} id="disco-map" alt="logo" />
         <img src={mondo1} className="App-logo" alt="logo" />
-
       </header>
       <div>
         
         <p>{this.state.apiResponse}</p>
         <iframe width="914" height="514" src="https://www.youtube.com/embed/a768UxZnr68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
         <main>
           <button>
             <ion-icon name="logo-facebook"></ion-icon>
@@ -148,6 +263,11 @@ render(){
           <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square"></i></a>
           <a href="http://34.211.44.78/" target="_blank"><i class="fab fa-dribbble"></i></a>
         </div>
+
+        <div class="corner-left">
+          <div class="g-ytsubscribe" data-channelid="UCVp_cqz0UCKFUzURdcqTj7g" data-layout="full" data-theme="dark" data-count="default"></div>
+        </div>
+
       </div>
     
     </div>
