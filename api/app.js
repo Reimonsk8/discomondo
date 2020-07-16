eb64a39dcc077e8f3c6833f2a111a271bbbb008b
reimon <reimonsk8@gmail.com>
2020-07-15 23:03:54 -0700

    updates only logo

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require("cors")
+var cors = require("cors");
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+var testJSONRouter = require('./routes/testJSON');
+var testRESRouter = require('./routes/testRES');
 var app = express();
 
 // view engine setup
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI',testAPIRouter);
+app.use('/testJSON', testJSONRouter);
+app.use('/testRES', testRESRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import mondo0 from './assets/mondo0.png';
+//import mondo0 from './assets/mondo0.png';
 import mondo1 from './assets/mondo1.png';
 import discomap from './assets/DISCO MONDO MAPA.png'
 import discologo from './assets/logo disco mondo solido.png'
@@ -8,49 +8,46 @@ import './styles/App.css';
 import './styles/magnetic.css';
 import $ from 'jquery';
 
+
 class App extends React.Component{
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={apiResponse:""};
+    this.state = { apiResponse: "" };
+    
+    this.callHome = this.callHome.bind(this);
+    this.callRes = this.callRes.bind(this);
+    this.callJSON = this.callJSON.bind(this);
   }
 
-  callAPI(){
-    fetch("http://localhost:5000/testAPI")
-      .then(res=> res.text())
-      .then(res=> res.setState({apiResponse: res}));
+  callHome(){
+    fetch("/")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: " " }));
   }
 
-
-
-
-
-
-
-  componetWillMount(){
-    this.callAPI();
+  callRes(){
+    fetch("/testRES")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
   }
 
+  callJSON(){
+    fetch("/testJSON")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
+  componentWillMount(){
+    //this.callHome();
+  }
 
 
 
   componentDidMount(){
     
-    console.clear();
+    //console.clear();
     
-    var imgs = [ discologo, discologocon]
+    var imgs = [discologo, discologocon]
     var spins = 0;
     var item = $("#disco-logo") 
     spin(item);
@@ -64,163 +61,8 @@ class App extends React.Component{
       
       setTimeout(function(){
         spin(target);
-      },2000);
+      },3000);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const { gsap } = window;
-
-    const cursorOuter = document.querySelector(".cursor--large");
-    const cursorInner = document.querySelector(".cursor--small");
-    let isStuck = false;
-    let mouse = {
-      x: -100,
-      y: -100,
-    };
-
-    // Just in case you need to scroll
-    let scrollHeight = 0;
-    window.addEventListener('scroll', function(e) {
-      scrollHeight = window.scrollY
-    })
-
-    let cursorOuterOriginalState = {
-      width: cursorOuter.getBoundingClientRect().width,
-      height: cursorOuter.getBoundingClientRect().height,
-    };
-    const buttons = document.querySelectorAll("main button");
-
-    buttons.forEach((button) => {
-      button.addEventListener("pointerenter", handleMouseEnter);
-      button.addEventListener("pointerleave", handleMouseLeave);
-    });
-
-    document.body.addEventListener("pointermove", updateCursorPosition);
-    document.body.addEventListener("pointerdown", () => {
-      gsap.to(cursorInner, 0.15, {
-        scale: 2,
-      });
-    });
-    document.body.addEventListener("pointerup", () => {
-      gsap.to(cursorInner, 0.15, {
-        scale: 1,
-      });
-    });
-
-    function updateCursorPosition(e) {
-      mouse.x = e.pageX;
-      mouse.y = e.pageY;
-    }
-
-    function updateCursor() {
-      gsap.set(cursorInner, {
-        x: mouse.x,
-        y: mouse.y,
-      });
-
-      if (!isStuck) {
-        gsap.to(cursorOuter, {
-          duration: 0.15,
-          x: mouse.x,
-          y: mouse.y,
-        });
-      }
-
-      requestAnimationFrame(updateCursor);
-    }
-
-    updateCursor();
-
-    function handleMouseEnter(e) {
-      isStuck = true;
-      const targetBox = e.currentTarget.getBoundingClientRect();
-      gsap.to(cursorOuter, 0.2, {
-        x: targetBox.left + targetBox.width / 2,
-        y: (targetBox.top + targetBox.height / 2) + scrollHeight,
-        width: targetBox.width,
-        height: targetBox.width,
-        borderRadius: 0,
-        backgroundColor: "rgba(255, 255, 255, 0.1)",
-      });
-    }
-
-    function handleMouseLeave(e) {
-      isStuck = false;
-      gsap.to(cursorOuter, 0.2, {
-        width: cursorOuterOriginalState.width,
-        height: cursorOuterOriginalState.width,
-        borderRadius: "50%",
-        backgroundColor: "transparent",
-      });
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
   }
 
@@ -228,49 +70,10 @@ render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={discologo} id="disco-logo" alt="logo" />
-        <img src={discomap} id="disco-map" alt="logo" />
-        <img src={mondo1} className="App-logo" alt="logo" />
       </header>
-      <div>
-        
-        <p>{this.state.apiResponse}</p>
-        <iframe width="914" height="514" src="https://www.youtube.com/embed/a768UxZnr68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        
-        <main>
-          <button>
-            <ion-icon name="logo-facebook"></ion-icon>
-          </button>
-          <button>
-            <ion-icon name="logo-twitter"></ion-icon>
-          </button>
-          <button>
-            <ion-icon name="logo-instagram"></ion-icon>
-          </button>
-          <button>
-            <ion-icon name="logo-youtube"></ion-icon>
-          </button>
-          <button>
-            <ion-icon name="logo-dribbble"></ion-icon>
-          </button>
-        </main>
-
-        <div class="cursor cursor--large"></div>
-        <div class="cursor cursor--small"></div>
-
-
-        <div class="support">
-          <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square"></i></a>
-          <a href="http://34.211.44.78/" target="_blank"><i class="fab fa-dribbble"></i></a>
-        </div>
-
-        <div class="corner-left">
-          <div class="g-ytsubscribe" data-channelid="UCVp_cqz0UCKFUzURdcqTj7g" data-layout="full" data-theme="dark" data-count="default"></div>
-        </div>
+        <img src={discologo} id="disco-logo" alt="logo" onClick={this.callHome}/>
 
       </div>
-    
-    </div>
   );
   }
 }
